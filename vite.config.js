dfbd81de7189e98f579ddadb767103630a07ce2a
Sajawal Mirza <mirzasajawal827@gmail.com>
2024-10-18 21:18:49 +0500

    Added GitHub Pages and Formatted the Code

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Portfolio/",
   plugins: [react()],
   server: {
-    open:true,port:9999
+    open: true,
+    port: 9999
   }
 })
